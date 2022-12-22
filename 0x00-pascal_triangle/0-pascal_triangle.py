1f2e52f96d49a97e563c972235fab2fe16dad7eb
middo1 <hamidadeshina1@gmail.com>
2022-12-22 20:26:41 +0100

    leggo

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+pascal triangle
+"""
+
+import sys
+
+def fat(num):
+	"""
+	function to return the factorial of num 
+	"""
+	if (num == 1 or num == 0):
+		return 1
+	else:
+		return num * fat(num - 1)
+
+def pascal_triangle(num):
+	container = []
+	for x in range(num):
+		container.append([])
+		for y in range(x+1):
+			container[x].append(int(fat(x)/(fat(x-y)*fat(y))))
+	return container
+
+
+			
+
+
+
+# print(pascal_triangle(int(sys.argv[1])))
