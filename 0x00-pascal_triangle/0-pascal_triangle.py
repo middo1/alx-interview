b843bee823c2731fa187d8622a270233714e3edd
middo1 <hamidadeshina1@gmail.com>
2022-12-22 20:47:24 +0100

    legoo

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -5,26 +5,25 @@ pascal triangle
 
 import sys
 
+
 def fat(num):
-	"""
-	function to return the factorial of num 
-	"""
-	if (num == 1 or num == 0):
-		return 1
-	else:
-		return num * fat(num - 1)
+    """
+function to return the factorial of num
+    """
+
+    if (num == 1 or num == 0):
+        return 1
+    else:
+        return num * fat(num - 1)
+
 
 def pascal_triangle(num):
-	container = []
-	for x in range(num):
-		container.append([])
-		for y in range(x+1):
-			container[x].append(int(fat(x)/(fat(x-y)*fat(y))))
-	return container
-
-
-			
-
-
-
-# print(pascal_triangle(int(sys.argv[1])))
+    """
+pascal triangle na
+    """
+    container = []
+    for x in range(num):
+        container.append([])
+        for y in range(x+1):
+            container[x].append(int(fat(x)/(fat(x-y)*fat(y))))
+    return container
