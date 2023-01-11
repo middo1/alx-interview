ae4a5eba07850d6dc56a6b287312b7f9a59a6d43
middo1 <hamidadeshina1@gmail.com>
2023-01-11 23:30:21 +0100

    leggo

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+A method that calculates the fewest number of operations
+needed to result in exactly n H characters in the file
+"""
+
+
+def minOperations(n):
+    """
+    calculates the fewest number of operations
+    """
+    h = 1
+    x = 1
+    counter = 0
+
+    if n < 2:
+        return 0
+    while h < n:
+        if n % h == 0:
+            x = h
+            h *= 2
+            counter += 2
+            print(counter, h)
+        else:
+            h += x
+            counter += 1
+            print(counter, h)
+    return counter
